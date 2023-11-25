e6c8ad0bccfc0999affc7aa45af4b3fdf7d28d90
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2023-11-25 22:20:15 +0330

    an other cod

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,55 @@
 
+import { Link } from "react-router-dom";
+
+
+
+
+
+
+
 
 const Login = () => {
-    return (
-        <div className="container flex-center">
-           <form className="form">
-             {/* email */}
-             <div className="box flex-cols">
-              <label className="label" htmlFor="email">email</label>
-              <input className="input" type="email" />
-             </div>
-             {/* password */}
-             <div className="box flex-cols">
-              <label className="label" htmlFor="password">password</label>
-              <input className="input" type="password" />
-             </div>
-              <button className="button" type="submit">Login</button>
-              <div>
-                <p>if you do not have an account register now</p>
-              </div>
-           </form>
-           {/* box right */}
-           <div className="hidden md:flex-center-col boxR">
-            <p className="boxChild">Welcome</p>
-            <span className="boxChild">Hi! are you Ready to start</span>
-           </div>
+
+
+
+  return (
+    <div className="container flex-center">
+      <form className="form">
+        {/* email */}
+        <div className="box flex-cols">
+          <label className="label" htmlFor="email">
+            email
+          </label>
+          <input className="input" type="email" />
         </div>
-    );
-}
+        {/* password */}
+        <div className="box flex-cols">
+          <label className="label" htmlFor="password">
+            password
+          </label>
+          <input className="input" type="password" />
+        </div>
+        <button className="button" type="submit">
+          Login
+        </button>
+      </form>
+      {/* box right */}
+      <div className="hidden md:flex-center-col boxR">
+        <p className="boxChild">Welcome</p>
+        <span className="boxChild">Login and start</span>
+        <p className="boxChild">if you do not have an account ?</p>
+        <div className="flex   flex-row-reverse mt-6  ">
+          <Link to={"/singup"} className="text-blue-400" href="#">
+            Register
+          </Link>
+          <span className="relative flex h-1 w-1">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-1 w-1 bg-sky-500"></span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- src/components/SingUp.jsx
+++ src/components/SingUp.jsx
@@ -1,33 +1,55 @@
 
+import { useFormik } from "formik";
+
 
 const SingUp = () => {
+
+    const formik=useFormik({
+        initialValues:{
+         name:"",
+         email:"",
+         password:"",
+         confirmPassword:"",
+         check:true,
+        },
+     
+        onSubmit:values => {
+         console.log(values)
+        }
+       })
+     
     return (
         <div className="container flex-center">
            <form className="form">
              {/* name */}
               <div className="box flex-cols">
               <label className="label" htmlFor="">name</label>
-              <input className="input" type="name" />
+              <input className={(formik.errors.name && formik.touched.name) ? "input2" :"input"} id="name" type="name" {...formik.getFieldProps('name')} />
+              {formik.errors.name && formik.touched.name && <span className="errors">{formik.errors.name}</span>}
              </div>
              {/* email */}
              <div className="box flex-cols">
               <label className="label" htmlFor="email">email</label>
-              <input className="input" type="email" />
+              <input className={(formik.errors.email && formik.touched.email) ? "input2" :"input"} id="email" type="email" {...formik.getFieldProps('email')} />
+              {formik.errors.email && formik.touched.email && <span className="errors">{formik.errors.email}</span>}
              </div>
              {/* password */}
              <div className="box flex-cols">
               <label className="label" htmlFor="password">password</label>
-              <input className="input" type="password" />
+              <input className={(formik.errors.password && formik.touched.password) ? "input2" :"input"} id="password" type="password" {...formik.getFieldProps('password')} />
+              {formik.errors.password && formik.touched.password && <span className="errors">{formik.errors.password}</span>}
              </div>
              {/* confirmPassword */}
               <div className="box flex-cols">
               <label className="label" htmlFor="confirmPassword">confirmPassword</label>
-              <input className="input" type="password" id="confirmPassword" />
+              <input className={(formik.errors.confirmPassword && formik.touched.confirmPassword) ? "input2" :"input"}  type="password" id="confirmPassword" {...formik.getFieldProps('confirmPassword')} />
+              {formik.errors.confirmPassword && formik.touched.confirmPassword && <span className="errors">{formik.errors.confirmPassword}</span>}
              </div>
 
              <div className="box flex-justify-start">
               <label className="label" htmlFor="check">I accept terms & conditions</label>
-              <input className="checkbox" type="checkbox" id="check" />
+              <input className="checkbox"  type="checkbox" id="check" {...formik.getFieldProps('check')} />
+              {formik.errors.check && formik.touched.check && <span className="checkErrors">{formik.errors.check}</span>}
              </div>
               <button className="button" type="submit">submit</button>
            </form>
